3f9d6725f6d1fab4cfe5e28caece6919903b6a05
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-04-25 21:57:35 +0500

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "home#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  unauthenticated do
+    root "users#index"
+  end
+
+  root 'groups#index', as: "moneytrack"
 
   resources :groups, only: [:index, :show, :new, :create, :destroy] do
-    resources :money_tracks, only: [ :new, :show, :create]
+    resources :money_tracks, only: [ :new, :show, :create, :destroy]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
